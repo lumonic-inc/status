36a1fa3492123e205953e6672822811312ef7a25
Andrew Riess <riessandrew@gmail.com>
2023-11-02 16:33:49 -0700

    permission fix

--- src/services/hooks/useSystemStatus.tsx
+++ src/services/hooks/useSystemStatus.tsx
@@ -65,7 +65,7 @@ function useSystemStatus() {
 }
 
 async function logs(key: string): Promise<ServiceStatus> {
-    const response = await fetch(`https://raw.githubusercontent.com/lumonic-inc/status/main/public/status/${key}_report.log`);
+    const response = await fetch(`https://raw.githubusercontent.com/lumonic-inc/status/master/public/status/${key}_report.log`);
     const text = await response.text();
     const lines = text.split("\n");
     try {
